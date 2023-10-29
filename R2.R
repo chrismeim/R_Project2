0388d8aa4ff84922b601c5afc9b9fa30e5ba985e
chrismeim <144551793+chrismeim@users.noreply.github.com>
2023-10-29 21:18:01 +0000

    Update R2.R

--- R2.R
+++ R2.R
@@ -1,6 +1,6 @@
 Introduction
 
-The aim of this excercise is to ...
+The aim of this excercise is to make some simple calculations on a dataframe that consists a large portfolio of securities.
 
 In the first part of the excercise I create a new dataframe containing the maximum daily return for each month, for each stock.
 Each row in the data represents the maximum daily return in a given month, for a given stock
